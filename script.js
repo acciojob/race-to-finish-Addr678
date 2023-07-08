1997243bd5f28988f33c5a6d6bba02bc47d75b3b
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-08 14:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-//window.promises = [];
-const promises = [
-  createPromise(1, 5),
-  createPromise(1, 5),
-  createPromise(1, 5),
-  createPromise(1, 5),
-  createPromise(1, 5),
-];
-
-const createPromise = (min, max) => {
-  const delay = Math.floor(Math.random() * (max - min + 1) + min) * 1000;
-
-  return new Promise((resolve) => {
+window.promises = [];
+ 
+// Create an array of five promises with random delays between 1 and 5 seconds
+for (let i = 0; i < 5; i++) {
+  const delay = Math.floor(Math.random() * 5) + 1;
+  const promise = new Promise((resolve) => {
     setTimeout(() => {
-      resolve(delay);
-    }, delay);
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
   });
-};
-
+  promises.push(promise);
+}
+ 
+// Wait for the first promise to resolve using Promise.any()
 Promise.any(promises)
   .then((result) => {
+    // Print the result to the output div
     const outputDiv = document.getElementById('output');
-    outputDiv.textContent = `Resolved with delay: ${result} ms`;
+    outputDiv.textContent = result;
   })
   .catch((error) => {
-    console.error('Promise.any error:', error);
+    // Handle errors if no promise resolves
+    console.log('All promises rejected:', error);
   });
-
-// Do not change the code above this
-// add your promises to the array `promises`
